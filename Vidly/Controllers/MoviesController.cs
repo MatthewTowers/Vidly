ca6034b99c0e2211bbbb7e87b1fe4ff38089e918
MatthewTowers <matthewtowers4454@gmail.com>
2018-07-28 20:39:59 +0200

    Finished up with the Working with data section

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -5,81 +5,37 @@ using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
 using Vidly.ViewModels;
+using System.Data.Entity;
 
 namespace Vidly.Controllers
 {
     public class MoviesController : Controller
     {
+        private ApplicationDbContext _context;
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         public ViewResult Index()
         {
-            var movies = GetMovies();
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
 
             return View(movies);
         }
 
-        public ActionResult Edit(int id)
+        public ActionResult Details(int id)
         {
-            return Content("id=" + id);
-        }
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(c => c.Id == id);
 
-        // movies
-        
-       /* public ActionResult Index(int? pageIndex, string sortBy)
-        {
-            if (!pageIndex.HasValue)
-                pageIndex = 1;
+            if (movie == null)
+                return HttpNotFound();
 
-            if (String.IsNullOrWhiteSpace(sortBy))
-                sortBy = "Name";
-
-            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
-        }
-        */
-
-        [Route("movies/released/{year:regex(^\\d{4}$)}/{month:regex(^\\d{2}$):range(1, 12)}")]
-        public ActionResult ByReleaseDate(int year, byte month)
-        {
-            return Content(year + "/" + month);
-        }
-
-
-        private IEnumerable<Movie> GetMovies()
-        {
-            return new List<Movie>
-            {
-                new Movie { Id = 1, Name = "Shrek" },
-                new Movie { Id = 2, Name = "Wall-e" }
-            };
-        }
-
-        // GET: Movies
-        public ViewResult Random()
-        {
-            var movie = new Movie() { Name = "Shrek!" };
-            var customers = new List<Customer>
-            {
-                new Customer {Name = "Customer 1" },
-                new Customer { Name = "Customer 2" }
-            };
-
-            var viewModel = new RandomMovieViewModel
-            {
-                Movie = movie,
-                Customers = customers
-            };
-
-            return View(viewModel);
-
-
-
-
-            //return Content("Hello World!");
-            // return HttpNotFound();
-            // return new EmptyResult();
-            // return RedirectToAction("Index", "Home", new { page = 1, sortBy = "name" } );
-            // ViewData["Movie"] = movie;
-            // ViewBag.Movie = movie;
-
+            return View(movie);
         }
     }
 }
---
+++ Vidly/Models/MembershipType.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        public byte Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Name { get; set; }
+        public short SignupFee { get; set; }
+        public byte DurationInMonths { get; set; }
+        public byte DiscountRate { get; set; }
+    }
+}
